ecd45380eca3222a64f9c1d5d5913c9066c5dc4b
Moses Kim <mosesrandykim@gmail.com>
2021-08-11 17:22:41 -0700

    added solution for richest customer wealth

---
+++ richest-customer-wealth.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[][]} accounts
+ * @return {number}
+ */
+var maximumWealth = function (accounts) {
+  let maximumWealth = 0;
+  for (let i = 0; i < accounts.length; i++) {
+    let wealth = 0;
+    for (let j = 0; j < accounts[i].length; j++) {
+      wealth += accounts[i][j];
+    }
+    if (wealth > maximumWealth) {
+      maximumWealth = wealth;
+    }
+  }
+  return maximumWealth;
+};
