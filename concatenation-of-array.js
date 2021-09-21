bd6bb4bada5792feb05f45aef5429eaeaecb52df
Moses Kim <mosesrandykim@gmail.com>
2021-09-21 16:26:26 -0700

    added solution for concatenation of array

--- concatenation-of-array.js
+++ concatenation-of-array.js
@@ -29,3 +29,17 @@ n == nums.length
 1 <= n <= 1000
 1 <= nums[i] <= 1000
 */
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function (nums) {
+  const ans = [];
+  for (let i = 0; i < nums.length; i++) {
+    ans.push(nums[i]);
+  }
+  for (let j = 0; j < nums.length; j++) {
+    ans.push(nums[j]);
+  }
+  return ans;
+};
